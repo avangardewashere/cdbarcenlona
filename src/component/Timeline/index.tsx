6181b7879dc16a62e9767f80df0f32dc07b49c81
avangardewashere <avelpanaligan@gmail.com>
2024-11-15 18:15:31 +0800

    design/timeline-indicator-design

--- src/component/Timeline/index.tsx
+++ src/component/Timeline/index.tsx
@@ -1,35 +1,36 @@
-import { memo } from 'react';
-import clsx from 'clsx';
-import style from './index.module.scss';
-import { Steps } from 'antd-mobile';
-const { Step } = Steps
+import { memo } from "react";
+import clsx from "clsx";
+import style from "./index.module.scss";
+import { Steps } from "antd-mobile";
+const { Step } = Steps;
 const TimeLine = memo(() => {
   return (
-	<div className={clsx(style.container)}>
-	 <Steps
-          direction='vertical'
-          current={1}
-        //   style={{
-        //     '--title-font-size': '17px',
-        //     '--description-font-size': '15px',
-        //     '--indicator-margin-right': '12px',
-        //     '--icon-size': '22px',
-        //   }}
-        >
-			<Step
-			title={"hello"}
-			description={
-				<div style={{color:"pink"}}>Pink</div>
-			}
-			/>
-				<Step
-			title={"hello"}
-			description={
-				<div style={{color:"pink"}}>Pink</div>
-			}
-			/>
-			</Steps>
-	</div>
+    <div className={clsx(style.container)}>
+      <Steps
+        direction="vertical"
+        current={1}
+        className={clsx(style.employmentTimeline)}
+        style={
+          {
+            //     '--title-font-size': '17px',
+            //     '--description-font-size': '15px',
+            //     '--indicator-margin-right': '12px',
+               '--icon-size': '60rem',
+          }
+        }
+      >
+        <Step
+          className={clsx(style.period)}
+          title={"hello"}
+          description={<div style={{ color: "pink" }}>Pink</div>}
+        />
+        <Step
+          className={clsx(style.period)}
+          title={"hello"}
+          description={<div style={{ color: "pink" }}>Pink</div>}
+        />
+      </Steps>
+    </div>
   );
 });
 
